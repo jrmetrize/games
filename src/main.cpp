e814389b76b96ac7ecbd0d84cea9cc2b5af6b13b
jrmetrize <jrmetrize@gmail.com>
2021-01-01 06:47:42 -0500

    add imgui

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <string>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 void
 error_dialog(std::string error_message);
 
@@ -21,19 +25,86 @@ main(int argc, const char **argv)
 {
   GLFWwindow *window;
 
-  glfwInit();
-  window = glfwCreateWindow(1280, 720, "fp2d", NULL, NULL);
+  if (glfwInit() != GLFW_TRUE)
+  {
+    error_dialog("Couldn't initialize GLFW.");
+    return 0;
+  }
+
+  window = glfwCreateWindow(1280, 720, "fp2d", nullptr, nullptr);
+  if (window == nullptr)
+  {
+    error_dialog("Couldn't create window.");
+    glfwTerminate();
+    return 0;
+  }
+
   glfwMakeContextCurrent(window);
-  gladLoadGL();
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    error_dialog("Couldn't initialize OpenGL.");
+    glfwTerminate();
+    return 0;
+  }
+
+  /* tmp */
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 130");
+
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   while (!glfwWindowShouldClose(window))
   {
-    glClearColor(1, 0, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
     glfwPollEvents();
+
+    ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            //ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
   }
 
   glfwTerminate();
+
   return 0;
 }
